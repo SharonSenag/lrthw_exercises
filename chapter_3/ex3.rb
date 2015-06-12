de1ac1913d9385ecb01a5097aa652977893f27fb
Sharon Sena <sharon.sena.g@gmail.com>
2015-06-12 16:06:40 -0600

    Modified chapter 3 ex3.rb
    [#95933900]

--- chapter_3/ex3.rb
+++ chapter_3/ex3.rb
@@ -1,19 +1,17 @@
 # Tell ruby to count chickens
-puts "I will now count my chickens:"
+print "I will now count my chickens: "
 
 # Add 25 plus 30 hens then divide by 6
-puts "Hens #{25 + 30 / 6}"
+print "Hens #{25 + 30 / 6.0}, "
 # Multiply 25 times 3 to the modulus of 4 then subtract from 100
 puts "Roosters #{100 - 25 * 3 % 4}"
 
 # Calculates the number of eggs
-puts "Now I will count the eggs:"
-
+print "Now I will count the eggs: "
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 # Prints the question then calculates answer
-puts "Is it true that 3 + 2 < 5 - 7?"
-
+print "Is it true that 3 + 2 < 5 - 7? "
 puts 3 + 2 < 5 - 7
 
 # Prints equation then {} interpolation command asks ruby to solve it
