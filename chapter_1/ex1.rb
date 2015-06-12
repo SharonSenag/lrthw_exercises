c05f7a08bcf2829e6ad21c75411c798ee249e5c9
Sharon Sena <sharon.sena.g@gmail.com>
2015-06-12 15:20:59 -0600

    Added Readme and .rb to chapter_1
    [#95933896]

---
+++ chapter_1/ex1.rb
@@ -0,0 +1,11 @@
+puts 'Hello World'
+puts 'Hello Again'
+puts 'I like typing this.'
+puts 'This is fun.'
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts 'I want my script to print another line'
+#when I place an `octothorpe` at the beginning of a line
+#it acts as a comment and makes the line not be interpreted by the command line
+#To print just one line of code, place an octothorpe in front of all, but one line of script
